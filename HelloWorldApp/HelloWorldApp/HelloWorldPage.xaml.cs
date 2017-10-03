71251141becd0c6765fb234b04a0862bb0fdbb93
sam.oliver <sam.oliver@SAMO>
2017-10-03 15:35:12 +0100

    Add project files.

---
+++ HelloWorldApp/HelloWorldApp/HelloWorldPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace HelloWorldApp
+{
+    public partial class HelloWorldPage : ContentPage
+    {
+        public HelloWorldPage()
+        {
+            InitializeComponent();
+        }
+        private async void GenerateNFCCodeButton_OnClicked(object sender, EventArgs e)
+        {
+            //var name = NameEntry.Text;
+
+            var NFC_Code = GenerateNFC_Code();
+            //await DisplayAlert("Greeting", $"Hello {name}!", "Howdy");
+            await DisplayAlert("NFC Code: ", $"{NFC_Code}!", "Thanks!");
+        }
+
+        private void PropertiesButton_OnClicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new SimplePage());
+        }
+
+
+        private String GenerateNFC_Code()
+        {
+            String NFC_Code = "13025151";
+
+            return NFC_Code;
+        }
+
+        private void sendNFC_Code()
+        {
+
+        }
+
+    }
+}
---
+++ HelloWorldApp/HelloWorldApp/SimplePage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace HelloWorldApp
+{
+    public partial class SimplePage : ContentPage
+    {
+        public SimplePage()
+        {
+            InitializeComponent();
+        }
+
+        private void HomeButton_OnClicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new HelloWorldPage());
+        }
+    }
+}
